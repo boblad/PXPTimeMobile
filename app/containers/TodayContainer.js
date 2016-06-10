a0447a873e803ecdd5d1ebf6b7bd86a9c2dec0ba
Bryant Oblad <bryant.oblad@gmail.com>
2016-06-09 21:41:11 -0600

    added time icon

--- app/containers/TodayContainer.js
+++ app/containers/TodayContainer.js
@@ -5,6 +5,7 @@ import { listEntries } from '../actions/EntryActions';
 import Menu from '../components/Menu';
 import TopBar from '../components/TopBar';
 import Loader from '../components/Loader';
+import timeIcon from './images/timeIcon.png';
 import { getDayEntries, toTimeString } from '../helpers/TimeHelpers';
 import React, {
   View, Text, StyleSheet, ScrollView,
@@ -36,7 +37,7 @@ class TodayContainer extends Component {
     })
     dispatch(listEntries(user.asyncKey, startDate, endDate));
   }
-  
+
   render() {
     const { results } = this.props.entries;
     let mondayEntries = getDayEntries(results, this.state.startDate);
@@ -115,6 +116,10 @@ class TodayContainer extends Component {
                           <View key={`bar-${i}`} style={styles.purchaseWrap}>
                             <View style={styles.purchase}>
                               <View style={styles.iconWrap}>
+                                <Image
+                                  style={styles.icon}
+                                  resizeMode="contain"
+                                  source={timeIcon}/>
                               </View>
                               <View style={styles.leftInfoWrap}>
                                 <Text style={styles.purchaseText}>
@@ -130,7 +135,6 @@ class TodayContainer extends Component {
                                 </Text>
                                 <Text style={styles.purchaseText}>
                                   {ent.entry.status} >
-
                                 </Text>
                               </View>
                             </View>
@@ -376,61 +380,3 @@ var styles = StyleSheet.create({
     fontSize: 14
   }
 });
-
-
-// class TodayContainer extends Component {
-//
-//   componentWillMount() {
-//     const { dispatch, user } = this.props;
-//     let startDate = moment().format('YYYY-MM-DD');
-//     let endDate = moment().add(2, 'days').format('YYYY-MM-DD');
-//     dispatch(listEntries(user.asyncKey, startDate, endDate));
-//   }
-//
-//   render() {
-//     let totalHours = 0;
-//     let totalMinutes = 0;
-//     let html = this.props.entries.results.map((ent, index) => {
-//       totalHours = totalHours + parseInt(ent.entry.hours, 10)
-//       totalMinutes = totalMinutes + parseInt(ent.entry.minutes, 10)
-//         return (
-//           <View key={index} style={styles.todayInputWrap}>
-//             <Text style={styles.todayInput}>
-//               {moment(ent.entry.created_at).format('MMMM Do YYYY')}
-//             </Text>
-//             <Text style={styles.todayInput}>{ent.client.public.name}</Text>
-//             <Text style={styles.todayInput}>{ent.board.public.name}</Text>
-//             <Text style={styles.todayInput}>{ent.card.public.name}</Text>
-//             <Text style={styles.todayInput}>{ent.entry.hours}:{ent.entry.minutes}</Text>
-//             <Text style={styles.todayInput}>{ent.entry.status}</Text>
-//           </View>
-//         )
-//       });
-//       let extraHours = Math.floor(totalMinutes/60);
-//       totalHours = totalHours + extraHours;
-//       totalMinutes = totalMinutes % 60;
-//       totalHours = normalizeTime(totalHours);
-//       totalMinutes = normalizeTime(totalMinutes);
-//     return (
-//       <View style={styles.container}>
-//         {
-//           this.props.loading.isLoading &&
-//           <Loader/>
-//         }
-//         <TopBar
-//           title="Today"
-//           onBackClick={() => this.props.router.toDashboard(this.state)} />
-//         <ScrollView style={styles.basicScroll}>
-//           { html }
-//         </ScrollView>
-//         {
-//           html &&
-//           <View style={styles.totalWrap}>
-//             <Text style={styles.totalText}>Total: {totalHours}:{totalMinutes}</Text>
-//           </View>
-//         }
-//         <Menu router={this.props.router}/>
-//       </View>
-//     );
-//   }
-// }
