9f4de7ff96e886dfa22e3b71f8ee519c405b9c55
Stephen Tippets <stephen.tippets@gmail.com>
2016-06-12 23:19:49 -0600

    Style login view

--- app/containers/LoginContainer.js
+++ app/containers/LoginContainer.js
@@ -1,29 +1,31 @@
 'use strict';
 
 import { bindActionCreators } from 'redux';
-import { styles } from '../assets/StyleSheet';
+
 import { iconStyles } from '../assets/IconStyles';
 import { activityStyles } from '../assets/ActivityStyle';
 import { connect } from 'react-redux';
 import { clearMessages } from '../actions/MessageActions';
 import { loginWithKey, loginWithCreds, setApiKey } from '../actions/LoginActions';
-import Pxplogo from '../assets/images/Pxplogo.png';
+import pxpLogo from './images/pxpLogo.png';
 import config from '../config';
 //import ActivityIndicator from '../Common/ActivityIndicator/';
 import Loader from '../components/Loader';
 import Success from '../components/alerts/Success';
 import Failure from '../components/alerts/Failure';
 import React, {
-  StyleSheet,
+  ActivityIndicatorIOS,
+  AsyncStorage,
   Component,
-  View,
+  Dimensions,
+  Image,
+  Modal,
+  StatusBar,
+  StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
-  Image,
-  AsyncStorage,
-  Modal,
-  ActivityIndicatorIOS
+  View
 } from 'react-native';
 
 
@@ -92,52 +94,65 @@ class LoginContainer extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.mainContainer}>
+        <StatusBar
+          barStyle="light-content"
+        />
+        <Image style={styles.backgroundImage} source={require('./images/loginBackground.jpg')} />
         {
           this.props.message.errorMessage &&
           <Failure
             message={this.props.message.errorMessage}
             hideModalClick={this.hideModalClick} />
         }
-        <Image source={Pxplogo} style={iconStyles.logoIcon}/>
-        <Text>Welcome to pxp time.</Text>
-        {
-          this.state.showTokenLogin &&
-          <View style={styles.inputWrap}>
-            <Text sytle={styles.inputLabel}>Api Key</Text>
-            <TextInput
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={styles.input}
-              onChangeText={this.handleTokenInputChange}/>
+
+        <View style={styles.contentWrapper}>
+          <Image source={pxpLogo} resizeMode="contain" style={styles.logoIcon}/>
+          <View style={styles.formWrapper}>
+            {
+              this.state.showTokenLogin &&
+              <View>
+                <TextInput
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  placeholder="API Key"
+                  style={styles.textInput}
+                  onChangeText={this.handleTokenInputChange}/>
+              </View>
+            }
+            {
+              !this.state.showTokenLogin &&
+              <View>
+                <View>
+                  <TextInput
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    placeholder="E-mail"
+                    style={styles.textInput}
+                    onChangeText={this.handleUserNameChange}/>
+                  <Image style={styles.basicIcon} resizeMode="contain" source={require('./images/emailIcon.png')} />
+                </View>
+                <View>
+                  <TextInput
+                    secureTextEntry={true}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    placeholder="Password"
+                    style={styles.textInput}
+                    onChangeText={this.handlePasswordChange}/>
+                  <Image style={styles.basicIcon} resizeMode="contain" source={require('./images/lockIcon.png')} />
+                </View>
+              </View>
+            }
           </View>
-        }
-        {
-          !this.state.showTokenLogin &&
-          <View style={styles.inputWrap}>
-            <Text sytle={styles.inputLabel}>Email</Text>
-            <TextInput
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={styles.input}
-              onChangeText={this.handleUserNameChange}/>
-            <Text sytle={styles.inputLabel}>Password</Text>
-            <TextInput
-              secureTextEntry={true}
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={styles.input}
-              onChangeText={this.handlePasswordChange}/>
-          </View>
-        }
-        <View style={styles.inputWrap}>
-          <TouchableOpacity style={styles.button} onPress={this.handleLoginClick}>
+          <TouchableOpacity style={styles.buttonBlue} onPress={this.handleLoginClick}>
             <Text style={styles.buttonText}>Login</Text>
           </TouchableOpacity>
+          <Text style={styles.orText}>- or -</Text>
+          <TouchableOpacity style={styles.buttonClear} onPress={this.handleAlternateClick}>
+            <Text style={styles.buttonText}>Sign In with {this.state.signInType}</Text>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity onPress={this.handleAlternateClick}>
-          <Text>Or Sign In with {this.state.signInType}</Text>
-        </TouchableOpacity>
       </View>
     );
   }
@@ -155,6 +170,81 @@ class LoginContainer extends Component {
     }
   }
 }
+
+const { width, height } = Dimensions.get('window');
+
+var styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
+  backgroundImage: {
+    width: width,
+    height: height
+  },
+  contentWrapper: {
+    width: width,
+    height: height,
+    position: 'absolute',
+    top: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  logoIcon: {
+    width: width/2,
+    height: width/3
+  },
+  mainText: {
+    color: '#FFFFFF',
+    fontSize: 30
+  },
+  formWrapper: {
+  },
+  textInput: {
+    height: 54,
+    backgroundColor: '#FFFFFF',
+    width: width-50,
+    borderRadius: 5,
+    color: '#A1A6BB',
+    paddingLeft: 46,
+    marginTop: 20
+  },
+  basicIcon: {
+    width: 16,
+    height: 16,
+    position: 'absolute',
+    top: 39,
+    left: 15
+  },
+  buttonClear: {
+    height: 46,
+    width: width-150,
+    backgroundColor: 'transparent',
+    borderRadius: 23,
+    borderColor: 'rgba(255, 255, 255, .6)',
+    borderWidth: .5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonBlue: {
+    height: 46,
+    width: width-150,
+    marginTop: 20,
+    backgroundColor: '#3A5999',
+    borderRadius: 23,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16
+  },
+  orText: {
+    color: '#FFFFFF',
+    fontSize: 14,
+    marginTop: 10,
+    marginBottom: 10
+  }
+});
 
 export default connect(state => ({
     loading: state.loading,
